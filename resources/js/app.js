70edc22304f3ed7638ef739b237d0ffbb289813d
unknown <aristeuwalker@gmail.com>
2022-07-15 00:29:06 +0100

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        Vue.use(plugin)
+
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
+
+InertiaProgress.init({ color: '#4B5563' });
